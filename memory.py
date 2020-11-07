14bcbc276c90b886bdf7bfdba3f97949ae3f98da
Jaroslaw Kaszczak <kaszczak.jaroslaw@outlook.com>
2020-11-07 16:17:46 +0100

    Implement gameplay.

--- memory.py
+++ memory.py
@@ -1,3 +1,4 @@
+import os
 import string
 import random
 
@@ -39,31 +40,55 @@ def get_random_letters(letters, board):
                 index = 0
                 random.shuffle(letters_list)
 
+
 def display_board(board):
     length = len(board[0])
     header = string.ascii_uppercase[:length]
 
     print("\n  " + header)
-    
+
     index = 1
 
     for row in board:
         row_to_print = str(index) + " " + "".join(row)
         print(row_to_print)
         index += 1
-    
+
+
 def get_coords(col, row):
+    cols = string.ascii_lowercase[:col]
+    rows = list(range(row))
+    rows = list(map(lambda x: str(x + 1), rows))
 
     user_input = input("Provide your coords (Eg. a1, b4, etc...): ")
+    while len(user_input) != 2 or user_input[0].lower() not in cols or user_input[1] not in rows:
+        print("Incorrect answer. Try again!")
+        user_input = input("Provide your coords (Eg. a1, b4, etc...): ")
 
-    cols = string.ascii_lowercase[:col]
+    x = ord(user_input[0].lower()) - 97
+    y = int(user_input[1]) - 1
+    return y, x
 
-    rows = list(range(row))
-    rows = map(lambda x: str(x+1), rows)
 
-    while len(user_input) != 2 or user_input[0].lower() not in cols and user_input[1] not in rows:
-        pass
+def uncover_item(hidden_board, visible_board, coords):
+    row = coords[0]
+    col = coords[1]
+    if visible_board[row][col] != "#":
+        raise ValueError("You try check the same element again!")
+    visible_board[row][col] = hidden_board[row][col]
 
+
+def hash_items(coords1, coords2, board):
+    board[coords1[0]][coords1[1]] = '#'
+    board[coords2[0]][coords2[1]] = '#'
+
+
+def has_won(board):
+    for row in board:
+        for item in row:
+            if item == '#':
+                return False
+    return True
 
 
 def main():
@@ -73,8 +98,21 @@ def main():
     amount_of_letters = int(col * row / 2)
     letters = string.ascii_lowercase[:amount_of_letters]
     get_random_letters(letters, hidden_board)
-    display_board(visible_board)
-    get_coords()
+    while not has_won(visible_board):
+        os.system("cls || clear")
+        display_board(visible_board)
+
+        coords1 = get_coords(col, row)
+        uncover_item(hidden_board, visible_board, coords1)
+        display_board(visible_board)
+
+        coords2 = get_coords(col, row)
+        uncover_item(hidden_board, visible_board, coords2)
+        display_board(visible_board)
+
+        if visible_board[coords1[0]][coords1[1]] != visible_board[coords2[0]][coords2[1]]:
+            hash_items(coords1, coords2, visible_board)
+            input("Letters unmatched! Try again!")
 
 
 def init_board(col, row):
